2019351e62fbfbd7fcaec7aa91eb8e51060a9b7f
Prateek Saxena <prateeksaxena1999@gmail.com>
2026-03-12 18:12:50 +0530

    feat(01-01): load Geist and JetBrains Mono via next/font, inject as CSS variables

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import { Geist, JetBrains_Mono } from 'next/font/google'
+
+const geist = Geist({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  display: 'swap',
+})
+
+const mono = JetBrains_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+  display: 'swap',
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${geist.variable} ${mono.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
